dad82d1a1dd5895331090f2a3d09ddab0f239dde
Rasmus Jonsson <rasmus@colburn.se>
2022-02-16 09:59:06 +0100

    footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -8,102 +8,82 @@
       pt-8
       pb-6
       pt-28
-      border-t-4 border-pink-dark-hero
+      border-t-4 border-pink-hero
       mt-10
     "
   >
     <div class="container mx-auto px-8 md:px-16">
-      <div class="flex flex-wrap">
-        <div class="w-full lg:w-6/12 px-8 md:px-16">
-          <h3 class="headline3">Kontakt</h3>
+      <div class="grid grid-cols-1">
+        <h4 class="headline4">Kontakt</h4>
 
-          <a href="mailto:info@slagforhjaltar.se">
-            <span class="text">info@slagforhjaltar.se </span></a
+        <a href="mailto:info@slagforhjaltar.se">
+          <span class="text">info@slagforhjaltar.se </span></a
+        >
+
+        <h4 class="mt-6 headline4">Adress</h4>
+        <span class="text"> Adress: Rååvägen 39 25270 Råå </span>
+        <div class="mt-6">
+          <button
+            @click="
+              $_handleRouting(
+                'https://www.linkedin.com/company/slagforhjaltar/',
+                true
+              )
+            "
+            class="
+              bg-white
+              text-blue-400
+              shadow-lg
+              font-normal
+              h-10
+              w-10
+              items-center
+              justify-center
+              align-center
+              rounded-full
+              outline-none
+              focus:outline-none
+              mr-2
+              p-3
+            "
+            type="button"
           >
-
-          <h3 class="mt-6 headline3">Adress</h3>
-          <span class="text"> Adress: Rååvägen 39 25270 Råå </span>
-          <div class="mt-6">
-            <button
-              @click="
-                $_handleRouting(
-                  'https://www.linkedin.com/company/slagforhjaltar/',
-                  true
-                )
-              "
-              class="
-                bg-white
-                text-blue-400
-                shadow-lg
-                font-normal
-                h-10
-                w-10
-                items-center
-                justify-center
-                align-center
-                rounded-full
-                outline-none
-                focus:outline-none
-                mr-2
-                p-3
-              "
-              type="button"
-            >
-              <client-only>
-                <font-awesome-icon
-                  class="inline pb-3"
-                  :icon="['fab', 'linkedin']"
-                />
-              </client-only></button
-            ><button
-              @click="
-                $_handleRouting(
-                  'https://www.facebook.com/slagforhjaltar/',
-                  true
-                )
-              "
-              class="
-                bg-white
-                text-blue-600
-                shadow-lg
-                font-normal
-                h-10
-                w-10
-                items-center
-                justify-center
-                align-center
-                rounded-full
-                outline-none
-                focus:outline-none
-                mr-2
-                p-3
-              "
-              type="button"
-              href="https://www.facebook.com/slagforhjaltar/"
-            >
-              <client-only>
-                <font-awesome-icon
-                  class="inline pb-3"
-                  :icon="['fab', 'facebook']"
-                />
-              </client-only>
-            </button>
-          </div>
-        </div>
-      </div>
-      <hr class="my-6 text-gray-3000" />
-      <div
-        class="flex flex-wrap items-center md:justify-between justify-center"
-      >
-        <div class="w-full md:w-4/12 px-8 md:px-16 mx-auto text-center">
-          <div class="text-sm text-gray-400 font-semibold py-1">
-            Copyright © {{ date }} COLBURN COLLABORATION AB.
-            <a
-              href="https://colburn.se"
-              class="text-gray-400 hover:text-gray-900"
-              >Colburn</a
-            >
-          </div>
+            <client-only>
+              <font-awesome-icon
+                class="inline pb-3"
+                :icon="['fab', 'linkedin']"
+              />
+            </client-only></button
+          ><button
+            @click="
+              $_handleRouting('https://www.facebook.com/slagforhjaltar/', true)
+            "
+            class="
+              bg-white
+              text-blue-600
+              shadow-lg
+              font-normal
+              h-10
+              w-10
+              items-center
+              justify-center
+              align-center
+              rounded-full
+              outline-none
+              focus:outline-none
+              mr-2
+              p-3
+            "
+            type="button"
+            href="https://www.facebook.com/slagforhjaltar/"
+          >
+            <client-only>
+              <font-awesome-icon
+                class="inline pb-3"
+                :icon="['fab', 'facebook']"
+              />
+            </client-only>
+          </button>
         </div>
       </div>
     </div>
