5e2c375b7165ab5d5ff834ea74be379ddd189f20
Rasmus Jonsson <rasmus@colburn.se>
2023-03-09 09:27:26 +0100

    trigger build

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,6 +51,7 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [{ src: '~/plugins/fontawesome/fontawesome.js', mode: 'client' }],
 
+  
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
